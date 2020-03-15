433f26c616132ce647c8f65e036078f96552babc
Daniel <daniep04@ucm.es>
2020-03-15 20:56:00 +0100

    Se guardan los datos en un csv pero hay que modificar el formato para que se entienda mejor

---
+++ Assets/Scripts/CollectData.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectData : MonoBehaviour
+{
+    public GameObject sword;
+    public Text accelText;
+
+    Vector3 accel;
+    float cont;
+
+    Data dataFile;
+
+    private void Start()
+    {
+        accel = new Vector3(0, 0, 0);
+        cont = 0;
+        dataFile = new Data();
+    }
+
+    private void FixedUpdate()
+    {
+        //Si JoyconMovement devuelve que se ha pulsado el botón b cuenta 2 segundos
+        if (sword.GetComponent<JoyconMovement>().getPressed())
+        {
+            //Se guarda la aceleración en cada momento que se detecte como botón pulsado
+            accel = sword.GetComponent<JoyconMovement>().getAccel();
+
+            //Suponemos que hay 50 fixedupdate por segundo
+            //Si el contador llega a 100 (unos 2 segundos), le indicará a JoyconMovement
+            //que ponga a false el pulsado y que no entre aquí
+            //Además señala que es la última linea y manda true para que no escriba coma al final
+            //Y el contador vuelve a su estado inicial
+            if (cont >= 100)
+            {
+                sword.GetComponent<JoyconMovement>().setNotPressed();
+                dataFile.Write(accel.x, accel.y, accel.z, true);
+                cont = 0;
+            }
+
+            //Si no han pasado los 2 segundos, sigue escribiendo los distintos valores
+            //de la aceleración
+            else
+            {
+                cont++;
+                accelText.text = accel.ToString();
+                dataFile.Write(accel.x, accel.y, accel.z, false);
+            }
+        }
+  
+    }
+}
---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data
+{
+
+    public System.IO.StreamWriter fileWriter;
+    string filePath;
+    string fileName = "Movements.csv";
+
+    public Data()
+    {
+        //Sets the path of the data file
+        filePath = Application.dataPath + @"\Data\" + fileName;
+        fileWriter = new System.IO.StreamWriter(filePath, false);
+
+        fileWriter.AutoFlush = true;
+
+        Init();
+
+    }
+
+    //Write the first line
+    public void Init()
+    {
+        fileWriter.WriteLine("accelX-accelY-accelZ");
+    }
+
+    public void Write(float accelX, float accelY, float accelZ, bool last)
+    {
+        if (!last)
+            fileWriter.WriteLine(accelX + "-" + accelY + "-" + accelZ + ",");
+        else
+            fileWriter.WriteLine(accelX + "-" + accelY + "-" + accelZ);
+    }
+
+}
+
