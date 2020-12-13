567bc912456f9cafd159ca3b572d6086bf12004f
Daniel <daniep04@ucm.es>
2020-12-13 21:16:04 +0100

    Modelo de la red neuronal añadido y script que lo carga

---
+++ Assets/Scripts/NeuralNetwork.cs
@@ -0,0 +1,36 @@
+﻿using Keras;
+using Keras.Layers;
+using Keras.Models;
+using Numpy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class NeuralNetwork
+    {
+       public int Movement(float [][] Xnew)
+        {
+            int move = -1;
+
+            //Carga el modelo ya entrenado
+            string model_path = Application.dataPath + @"\Red_Neuronal\Modelo\model.json";
+            string weights_path = Application.dataPath + @"\Red_Neuronal\Modelo\model.h5";
+
+            var model = Sequential.ModelFromJson(File.ReadAllText(model_path));
+            model.LoadWeight(weights_path);
+
+            //Predice el tipo de un movimiento dado y devuelve el valor
+            var ynew = model.Predict(Xnew);
+
+            move = (int)ynew[0];
+
+            return move;
+        }
+
+        
+    }
+}
+
