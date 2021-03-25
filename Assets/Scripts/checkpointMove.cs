1c46e3a3a1d74b39ecb06134271340425c948ab4
hector <hectoval@ucm.es>
2021-03-25 12:10:41 +0100

    Añadida rotacion en movimiento de personaje. Añadidas mecanicas de cortar troncos y coger pociones.

--- Assets/Scripts/checkpointMove.cs
+++ Assets/Scripts/checkpointMove.cs
@@ -6,52 +6,68 @@ using UnityEngine;
 public class checkpointMove : MonoBehaviour
 {
     private int pos;
+    private int obstaculo;
+    private int pocion;
+    private bool cortar;
     public GameObject[] posMaps;
+    public GameObject[] obstaculoMaps;
+    public GameObject[] pocionMaps;
 
-    //private Transform target;
-    //private float speed = 20f;
+    private float speed = 5f;
 
     // Start is called before the first frame update
     private void Start()
     {
         //Leemos el archivo para saber cuantos puntos hay en cada mapa
         pos = 0;
+        obstaculo = 0;
+        pocion = 0;
+        cortar = false;
         //Fijamos el primer punto para empezar a movernos hacia el
         transform.LookAt(GameObject.Find("punto0").transform);
+    }
+
+    private void Update()
+    {
+        if (cortar && Input.GetKeyDown("e"))
+        {
+            Destroy(obstaculoMaps[obstaculo]);
+            obstaculo++;
+            cortar = false;
+        }
+        Vector3 direction = posMaps[pos].transform.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
     }
 
     // Upon collision with another GameObject, this GameObject will reverse direction
     private void OnTriggerEnter (Collider other)
     {
         //actualizamos el contador de posicion y mientras haya posiciones a las que ir, cambiamos la direccion. Si no hay mas mensaje de que se acabo
-        pos++;
-        if (pos < posMaps.Length)
-        {
-            transform.LookAt(posMaps[pos].transform);
+        if (other.tag == "punto") {
+            pos++;
+            //if (pos < posMaps.Length)
+            //{
+            //    transform.LookAt(posMaps[pos].transform);
 
+            //}
+            //else
+            //{
+            //    Debug.Log("Fin del viaje");
+            //}
         }
-        else
+        else if (other.tag == "tronco")
         {
-            Debug.Log("Fin del viaje");
+            cortar = true;
+        }
+        else if (other.tag == "pocion")
+        {
+            Destroy(pocionMaps[pocion]);
+            pocion++;
         }
 
-        //target = GameObject.Find(nextpos).transform;
         //Vector3 direction = target.position - transform.position;
         //Quaternion rotation = Quaternion.LookRotation(direction);
         //transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
-        //transform.LookAt(GameObject.Find(nextpos).transform);
-
-
-        // Determine which direction to rotate towards
-        //Vector3 targetDirection = target.position - transform.position;
-
-        // The step size is equal to speed times frame time.
-        //float singleStep = speed * Time.deltaTime;
-
-        // Rotate the forward vector towards the target direction by one step
-        //Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
-
-        // Calculate a rotation a step closer to the target and applies rotation to this object
-        //transform.rotation = Quaternion.LookRotation(newDirection);
     }
 }
