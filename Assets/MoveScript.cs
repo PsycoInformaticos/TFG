77672d76b01ac5f7fea4644d5e55e1bbe8f49706
hector <hectoval@ucm.es>
2021-04-17 00:13:52 +0200

    Mejorada la funcionalidad del minijuego del runner, separados los colliders del jugador para poder manejarlos mas facilmente (falta pulirlo)

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    private int pos;
+    private int pocion;
+    public GameObject[] posMaps;
+    public GameObject[] pocionMaps;
+    private float speed = 5f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        pos = 0;
+        pocion = 0;
+        //Fijamos el primer punto para empezar a movernos hacia el
+        transform.LookAt(GameObject.Find("punto0").transform);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 direction = posMaps[pos].transform.position - transform.position;
+        direction.y = 0f;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //actualizamos el contador de posicion y mientras haya posiciones a las que ir, cambiamos la direccion. Si no hay mas mensaje de que se acabo
+        if (other.tag == "punto")
+        {
+            pos++;
+        }
+
+        else if (other.tag == "pocion")
+        {
+            //vida += 20;
+            Debug.Log("Gano vida");
+            //if (vida >= 100)
+            //{
+            //    vida = 100;
+            //}
+            Destroy(pocionMaps[pocion]);
+            pocion++;
+        }
+        else if (other.tag == "piedra")
+        {
+            //vida -= 10;
+            Debug.Log("Pierdo vida");
+            //if (vida <= 0)
+            //{
+            //game over
+            //}
+            Destroy(other.gameObject);
+        }
+    }
+}
--- Assets/Scripts/checkpointMove.cs
+++ Assets/Scripts/checkpointMove.cs
@@ -6,68 +6,26 @@ using UnityEngine;
 public class checkpointMove : MonoBehaviour
 {
     private int pos;
-    private int obstaculo;
     private int pocion;
-    private bool cortar;
     public GameObject[] posMaps;
-    public GameObject[] obstaculoMaps;
     public GameObject[] pocionMaps;
-
     private float speed = 5f;
 
     // Start is called before the first frame update
     private void Start()
     {
-        //Leemos el archivo para saber cuantos puntos hay en cada mapa
         pos = 0;
-        obstaculo = 0;
         pocion = 0;
-        cortar = false;
         //Fijamos el primer punto para empezar a movernos hacia el
-        transform.LookAt(GameObject.Find("punto0").transform);
+        GameObject.FindWithTag("Player").transform.LookAt(GameObject.Find("punto0").transform);
     }
 
     private void Update()
     {
-        if (cortar && Input.GetKeyDown("e"))
-        {
-            Destroy(obstaculoMaps[obstaculo]);
-            obstaculo++;
-            cortar = false;
-        }
         Vector3 direction = posMaps[pos].transform.position - transform.position;
+        direction.y = 0f;
         Quaternion rotation = Quaternion.LookRotation(direction);
         transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
-    }
 
-    // Upon collision with another GameObject, this GameObject will reverse direction
-    private void OnTriggerEnter (Collider other)
-    {
-        //actualizamos el contador de posicion y mientras haya posiciones a las que ir, cambiamos la direccion. Si no hay mas mensaje de que se acabo
-        if (other.tag == "punto") {
-            pos++;
-            //if (pos < posMaps.Length)
-            //{
-            //    transform.LookAt(posMaps[pos].transform);
-
-            //}
-            //else
-            //{
-            //    Debug.Log("Fin del viaje");
-            //}
-        }
-        else if (other.tag == "tronco")
-        {
-            cortar = true;
-        }
-        else if (other.tag == "pocion")
-        {
-            Destroy(pocionMaps[pocion]);
-            pocion++;
-        }
-
-        //Vector3 direction = target.position - transform.position;
-        //Quaternion rotation = Quaternion.LookRotation(direction);
-        //transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
     }
 }
