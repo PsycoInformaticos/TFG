9047245983d6ee7d54d1bba8d9a4547712884719
DPerezLuque <daniep04@ucm.es>
2021-04-21 18:20:07 +0200

    Escena del minijuego de slashing y código general de los puntos

---
+++ Assets/Scripts/Puntos.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Puntos : MonoBehaviour
+{
+
+    public Text t;
+    int points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = 0;
+        t.text = "Puntuación: " + points;
+    }
+
+
+    public void setPoints (int p)
+    {
+        points += p;
+        t.text = "Puntuación: " + points;
+    }
+}
