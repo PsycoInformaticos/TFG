307706eb26515e70632ca6b948ece9dcaab028c7
Daniel <daniep04@ucm.es>
2021-09-04 19:30:38 +0200

    Mejorado el minijuego slasher para adaptarse a lo diseñado

--- Assets/Scripts/Slasher Logic/Slashing.cs
+++ Assets/Scripts/Slasher Logic/Slashing.cs
@@ -6,16 +6,14 @@ using UnityEngine.UI;
 public class Slashing : MonoBehaviour
 {
 
-    public GameObject star;     //Esfera a cortar
-    public GameObject rSword;   //Espada derecha
-    public GameObject lSword;   //Espada izquierda
-    public GameObject points;   //Texto de los puntos
+    public GameObject pinkBot;      //Esfera rosa a cortar
+    public GameObject greenBot;     //Esfera verde a cortar
+    public GameObject rSword;       //Espada derecha
+    public GameObject lSword;       //Espada izquierda
+    public GameObject points;       //Texto de los puntos
 
-    public Text direccionR; //Texto para indicar hacia donde se mueve el mando
-    public Text direccionL; //Texto para indicar hacia donde se mueve el mando
-
-    public Material rMaterial;
-    public Material lMaterial;
+    //public Text direccionR; //Texto para indicar hacia donde se mueve el mando
+    //public Text direccionL; //Texto para indicar hacia donde se mueve el mando
 
     bool sphereActive;          //Determina si hay una esfera que cortar
     int nextStar;               //Tiempo para la siguiente aparicion de una estrella
@@ -30,85 +28,131 @@ public class Slashing : MonoBehaviour
         contNextStar = 0;
         checkMove = 50;
 
-        star.SetActive(false);
+        pinkBot.SetActive(false);
+        greenBot.SetActive(false);
     }
 
     private void Update()
     {
-        int moveTypeR = rSword.GetComponent<JoyconMovement>().moveType();
+        //int moveTypeR = rSword.GetComponent<JoyconMovement>().moveType();
         //int moveTypeL = lSword.GetComponent<JoyconMovement>().moveType();
 
-        switch (moveTypeR)
-        {
-            case 0:
-                direccionR.text = "UP";
-                break;
-            case 1:
-                direccionR.text = "DOWN";
-                break;
-            case 2:
-                direccionR.text = "RIGHT";
-                break;
-            case 3:
-                direccionR.text = "LEFT";
-                break;
-            case 4:
-                direccionR.text = "NONE";
-                break;
-        }
+        //switch (moveTypeR)
+        //{
+        //    case 0:
+        //        direccionR.text = "UP";
+        //        break;
+        //    case 1:
+        //        direccionR.text = "DOWN";
+        //        break;
+        //    case 2:
+        //        direccionR.text = "RIGHT";
+        //        break;
+        //    case 3:
+        //        direccionR.text = "LEFT";
+        //        break;
+        //    case 4:
+        //        direccionR.text = "NONE";
+        //        break;
+        //}
 
         //direccionR.text = moveTypeR.ToString();
         //direccionL.text = moveTypeL.ToString();
 
 
-        //if (sphereActive)
-        //{
-        //    if (checkMove >= 100)
-        //    {
-        //        if (isSlashing())
-        //        {
-        //            star.SetActive(false);
-        //            sphereActive = false;
-        //            points.GetComponent<Puntos>().setPoints(5);
+        if (sphereActive)
+        {
+            if (checkMove >= 100)
+            {
+                if (isSlashing())
+                {
+                    if(pinkBot.activeSelf) pinkBot.SetActive(false);
+                    else if (greenBot.activeSelf) greenBot.SetActive(false);
+                    sphereActive = false;
+                    points.GetComponent<Puntos>().setPoints(5);
 
-        //        }
+                }
 
-        //        contNextStar = 0;
-        //        checkMove = 0;
-        //    }
-        //    else checkMove++;
-        //}
-        //else
-        //{
-        
-        //    contNextStar++;
+                contNextStar = 0;
+                checkMove = 0;
+            }
+            else checkMove++;
+        }
+        else
+        {
 
-        //    if (contNextStar >= nextStar)
-        //    {
-        //        sphereActive = true;
-        //        star.SetActive(true);
+            contNextStar++;
 
-        //        Renderer render = star.GetComponent<Renderer>();
-        //        int r = Random.Range(0, 2);
-        //        if (r == 0) render.material = rMaterial;
-        //        else if (r == 1) render.material = lMaterial;
-        //    }
-        //}
+            if (contNextStar >= nextStar)
+            {
+                sphereActive = true;
+                randomBot();
+            }
+        }
     }
+
+    void randomBot() 
+    {
+        sphereActive = true;
+
+        int r = Random.Range(0, 4);
+        if (r == 0)
+        {
+            Vector3 angles = pinkBot.transform.rotation.eulerAngles;
+            angles.z = 0;
+            pinkBot.transform.rotation = Quaternion.Euler(angles);
+            pinkBot.SetActive(true);
+        }
+        else if (r == 1)
+        {
+            Vector3 angles = greenBot.transform.rotation.eulerAngles;
+            angles.z = 0;
+            greenBot.transform.rotation = Quaternion.Euler(angles);
+            greenBot.SetActive(true);
+        }
+        else if (r == 2)
+        {
+            Vector3 angles = pinkBot.transform.rotation.eulerAngles;
+            angles.z = 90;
+            pinkBot.transform.rotation = Quaternion.Euler(angles);
+            pinkBot.SetActive(true);
+        }
+        else if (r == 3)
+        {
+            Vector3 angles = greenBot.transform.rotation.eulerAngles;
+            angles.z = 90;
+            greenBot.transform.rotation = Quaternion.Euler(angles);
+            greenBot.SetActive(true);
+        }
+
+    }
+
 
     bool isSlashing()
     {
         int moveTypeR = rSword.GetComponent<JoyconMovement>().moveType();
         int moveTypeL = lSword.GetComponent<JoyconMovement>().moveType();
 
-        direccionR.text = moveTypeR.ToString();
-        direccionL.text = moveTypeL.ToString();
+        //direccionR.text = moveTypeR.ToString();
+        //direccionL.text = moveTypeL.ToString();
 
-        if (star.GetComponent<Renderer>().sharedMaterial == rSword.GetComponent<Renderer>().sharedMaterial && moveTypeR != 4)
+        //Si la esfera es rosa y se mueve la espada rosa en horizontal (2 = der, 3 = izq)
+        if ((pinkBot.activeSelf && pinkBot.transform.rotation.eulerAngles.z == 0) && (moveTypeR == 2 || moveTypeR == 3))
         {
             return true;
         }
-        else if (star.GetComponent<Renderer>().sharedMaterial == lSword.GetComponent<Renderer>().sharedMaterial && moveTypeL != 4)
+        //Si la esfera es rosa y se mueve la espada rosa en vertical (0 = arriba, 1 = abajo)
+        else if ((pinkBot.activeSelf && pinkBot.transform.rotation.eulerAngles.z == 90) && (moveTypeR == 0 || moveTypeR == 1))
+        {
+            return true;
+        }
+        //Si la esfera es verde y se mueve la espada verde en horizontal (2 = der, 3 = izq)
+        if ((greenBot.activeSelf && greenBot.transform.rotation.eulerAngles.z == 0) && (moveTypeL == 2 || moveTypeL == 3))
+        {
+            return true;
+        }
+        //Si la esfera es verde y se mueve la espada verde en vertical (0 = arriba, 1 = abajo)
+        else if ((greenBot.activeSelf && greenBot.transform.rotation.eulerAngles.z == 90) && (moveTypeL == 0 || moveTypeL == 1))
         {
             return true;
         }
